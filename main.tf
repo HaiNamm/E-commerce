74c025acd731c21785dff80134f70fd935512d28
HaiNamm <dhtn8xx@gmail.com>
2024-07-18 09:14:37 +0700

    first commit

---
+++ main.tf
@@ -0,0 +1,86 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+    region = "ap-southeast-1"
+    access_key = "AKIARPYYZ6U7A76NBHM7"
+    secret_key = "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"
+}
+
+resource "aws_security_group" "ec2_security_group" {
+  name        = "ec2 security group"
+  description = "allow access on ports 80 and 22"
+  vpc_id      = aws_vpc.myvpc.id
+
+  ingress {
+    description = "http access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "ssh access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ec2 security group"
+  }
+}
+
+resource "tls_private_key" "rsa_4096" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+variable "key_name" {
+
+}
+
+variable "instance_count" {
+  default = 3 
+}
+
+resource "aws_key_pair" "key_pair" {
+  key_name   = var.key_name
+  public_key = tls_private_key.rsa_4096.public_key_openssh
+}
+
+resource "local_file" "private_key" {
+  content = tls_private_key.rsa_4096.private_key_pem
+  filename = var.key_name
+}
+
+resource "aws_instance" "public_instance" {
+  count         = var.instance_count
+  ami           = "ami-060e277c0d4cce553"
+  instance_type = "t3.micro"
+  subnet_id              = aws_subnet.PublicSubnet.id
+  key_name = aws_key_pair.key_pair.key_name
+  security_groups = [aws_security_group.ec2_security_group.id]
+  tags = {
+    Name = "node${count.index + 1}"
+  }
+}
+
+output "ec2_public_ipv4_urls" {
+  value = [for idx, instance in aws_instance.public_instance : "http://${instance.public_dns}"]
+}
